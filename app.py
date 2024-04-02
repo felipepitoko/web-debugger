aa8d347ee1af48d2283691f4afdf72367412ac75
Felipe Costa <lip-sousa@hotmail.com>
2024-04-02 15:20:10 -0300

    fix: salvando incoming json

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask import jsonify
 
 app = Flask(__name__) 
+app.debug = True
 @app.route('/', methods=['POST', 'GET'])
 def index():
     try:
@@ -12,24 +13,26 @@ def index():
             #send "hello-world.html" from templates as response
             return render_template('hello-world.html')
         else:        
-            data = request.json
-
-            if data:
+            if request.is_json:
+                data = request.json
                 nome_arquivo = datetime.now().strftime('%d-%m-%Y %H-%M-%S')
                 with open(f"received/{nome_arquivo}.json", 'w') as file:
-                    json.dump(jsonify(data), file)
+                    json.dump(data, file)
 
                 return 'JSON data saved to file successfully'
             else:
                 return 'No JSON data found in the request body', 400
     except Exception as e:
         print(e)
-        return 'Internal server error', 500
+        return jsonify({
+            "error": "Internal server error",
+            "message": str(e)
+        }), 500
     
 @app.route('/teste', methods=['GET'])
 def teste_on():
-    return ({"teste": "ok"}, 200)
+    return ({"teste": "nah"}, 200)
 
 #set the app to auto reload everytime the code changes
 if __name__ == '__main__':    
-    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, debug=True)
